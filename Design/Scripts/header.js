a7babe518efb526ff7bd8da808f872a3522a4c0f
Zdenko Kanoš <xkanos@stuba.sk>
2025-03-13 00:20:01 +0100

    Header Responsivity (#16)
    * repaired the recommended products in index.css
    
    * removed logo from all headers, updated header border-raidus, updated header-padding in all files
    
    * completed header resposivity for all pages

---
+++ Design/Scripts/header.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.querySelector(".mobile-menu").classList.toggle("active");
+    document.querySelector(".menu-icon").classList.toggle("active");
+    document.querySelector(".close-icon").classList.toggle("active");
+    document.querySelector("body").classList.toggle("no-scroll");
+}
