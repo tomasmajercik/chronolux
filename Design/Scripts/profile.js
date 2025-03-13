4a7e73a4d179901cdfc2955d05b6ede279752bc5
Zdenko Kanoš <xkanos@stuba.sk>
2025-03-13 23:00:30 +0100

    made the profile more responsive, enough for today

---
+++ Design/Scripts/profile.js
@@ -0,0 +1,9 @@
+function closeSidebar() {
+    document.querySelector(".sidebar").classList.toggle("active");
+    
+    if (sidebar.classList.contains("active")) {
+        console.log("Sidebar is now HIDDEN");
+    } else {
+        console.log("Sidebar is now VISIBLE");
+    }
+}
