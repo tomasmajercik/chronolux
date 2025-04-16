c02a919f6150f9fc80c8d1cf6c6b98ef9be83eb0
Tomáš Majerčík <tomas.majercik2@gmail.com>
2025-04-16 16:33:03 +0200

    watch-category_preview added

---
+++ laravel/chronoLux/resources/views/components/watch-category-preview.blade.php
@@ -0,0 +1,7 @@
+<div class="category-description">
+    <div class="header-hidden-button">
+        <h1>{{ $categoryName }}</h1>
+        <button class="hidden-btn" onclick="window.location.href='{{ $link }}'">Shop</button>
+    </div>
+    <p>{{ $description }}</p>
+</div>
--- laravel/chronoLux/resources/views/home.blade.php
+++ laravel/chronoLux/resources/views/home.blade.php
@@ -1,25 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/product_page.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <title>WTECH-Watch</title>
-    <link rel="icon" href="IMGs/icon.png">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+@section('title', 'Ultra Luxury')
 
-<body>
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endpush
 
-<x-header>
+@section('header-slot')
     <div class="lower-header">
-        <h1 class="category-title">!!Blueprint!!</h1>
+        <p>
+            Your style is a powerful form of communication, make it matter.
+        </p>
+        <button onclick="location.href='#watch-category'">View More</button>
     </div>
-</x-header>
+@endsection
 
+@section('content')
+<main>
+    <!-- Watch Categories -->
+    <section class="watch-category" id="watch-category">
+        <div class="category-heading">
+            <h2>Watch categories</h2>
+            <button onclick="window.location.href='product_page.html'">View all products</button>
+        </div>
+        <div class="category-container">
 
-</body>
-</html>
+            <div class="category-card cc1">
+                <x-watch-category-preview
+                    categoryName="Entry Luxury"
+                    description="A perfect blend of elegance and affordability, this watch elevates everyday style with luxury details."
+                    link="/product-page"
+                />
+            </div>
+
+            <div class="category-card cc2">
+                <x-watch-category-preview
+                    categoryName="Basic Luxury"
+                    description="A sophisticated timepiece with exquisite craftsmanship, offering luxury beyond the basics for refined tastes."
+                    link="/product-page"
+                />
+            </div>
+
+            <div class="category-card cc3">
+               <x-watch-category-preview
+                    categoryName="Ultra Luxury"
+                    description="A masterpiece of innovation and precision, this ultra-luxury watch embodies the pinnacle of craftsmanship and design."
+                    link="/product-page"
+                />
+            </div>
+
+        </div>
+    </section>
+</main>
+@endsection
