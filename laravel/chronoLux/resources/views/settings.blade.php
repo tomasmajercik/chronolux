bee625915ce5dbad16cd736af4f2d02c7d2a57da
Zdenko Kanoš <xkanos@stuba.sk>
2025-04-16 19:13:22 +0200

    fixed issues finished settings, orders stays not checked (#48)

--- laravel/chronoLux/resources/views/settings.blade.php
+++ laravel/chronoLux/resources/views/settings.blade.php
@@ -5,6 +5,26 @@
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 @endpush
 
+@section('title', 'Settings')
+
 @section('content')
+<main>
     <x-sidebar :active="'settings'" />
+    <div class="settings">
+        <h1>Settings</h1>
+        <div class="settings-container">
+            <ul>
+                <li>
+                    <x-settings :settingText="'Change email'" />
+                </li>
+                <li>
+                    <x-settings :settingText="'Change password'" />
+                </li>
+                <li>
+                    <x-settings :settingText="'Log out'" />
+                </li>
+            </ul>
+        </div>
+    </div>
+</main>
 @endsection
