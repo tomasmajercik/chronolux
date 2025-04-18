5a048f96fa9ab809d70feab65dbfa193720965b6
Zdenko Kanoš <xkanos@stuba.sk>
2025-04-18 20:11:18 +0200

    Filtering and sorting(#52)
    * functioning filter
    
    * sort done

--- laravel/chronoLux/resources/views/partials/sort-modal.blade.php
+++ laravel/chronoLux/resources/views/partials/sort-modal.blade.php
@@ -6,18 +6,18 @@
         <div class="sort-modal-holder">
             <label for="sort-price">Sort by Price:</label>
             <select id="sort-price">
-                <option value="none">-- Select --</option>
-                <option value="low-to-high">Low to High</option>
-                <option value="high-to-low">High to Low</option>
+                <option value="none" {{ request('sort_price') === null ? 'selected' : '' }}>-- Select --</option>
+                <option value="low-to-high" {{ request('sort_price') === 'low-to-high' ? 'selected' : '' }}>Low to High</option>
+                <option value="high-to-low" {{ request('sort_price') === 'high-to-low' ? 'selected' : '' }}>High to Low</option>
             </select>
         </div>
 
         <div class="sort-modal-holder">
-            <label for="sort-brand">Sort by Brand:</label>
-            <select id="sort-brand">
-                <option value="none">-- Select --</option>
-                <option value="a-z">A - Z</option>
-                <option value="z-a">Z - A</option>
+            <label for="sort-name">Sort by Name:</label>
+            <select id="sort-name">
+                <option value="none" {{ request('sort_name') === null ? 'selected' : '' }}>-- Select --</option>
+                <option value="a-z" {{ request('sort_name') === 'a_z' ? 'selected' : '' }}>A - Z</option>
+                <option value="z-a" {{ request('sort_name') === 'z_a' ? 'selected' : '' }}>Z - A</option>
             </select>
         </div>
 
