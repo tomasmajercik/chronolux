050192522744a28f5e98171803ac90c1658361a0
Tomáš Majerčík <tomas.majercik2@gmail.com>
2025-04-20 00:10:22 +0200

    preparation for settings

--- laravel/chronoLux/resources/views/components/settings.blade.php
+++ laravel/chronoLux/resources/views/components/settings.blade.php
@@ -1,9 +1,14 @@
-<a href="#">
-    {{ $settingText }}
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="31" viewBox="0 0 16 31"
-        fill="none">
-        <path fill-rule="evenodd" clip-rule="evenodd"
-            d="M13.5427 16.4183L6.00008 23.7253L4.11475 21.8989L10.7147 15.5051L4.11475 9.11139L6.00008 7.28497L13.5427 14.5919C13.7927 14.8342 13.9331 15.1626 13.9331 15.5051C13.9331 15.8476 13.7927 16.1761 13.5427 16.4183Z"
-            fill="#6A6A6A" />
-    </svg>
-</a>
+<div class="setting-link" onclick="showHidden(event)">
+    <a href="#">
+        <p><b>{{ $settingText }}</b></p>
+        <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="16" height="31" viewBox="0 0 16 31" fill="none">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M13.5427 16.4183L6.00008 23.7253L4.11475 21.8989L10.7147 15.5051L4.11475 9.11139L6.00008 7.28497L13.5427 14.5919C13.7927 14.8342 13.9331 15.1626 13.9331 15.5051C13.9331 15.8476 13.7927 16.1761 13.5427 16.4183Z"
+                fill="#6A6A6A" />
+        </svg>
+    </a>
+    <div class="hidden-content hidden">
+        mnau mnau
+    </div>
+</div>
+
